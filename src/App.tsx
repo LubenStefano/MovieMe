3147e26579e9b720cd96399a43a7191ee0f95761
LubenStefano <lubenstefanovkn@gmail.com>
2025-06-11 23:31:22 +0300

    feat: Add About page with styling and content; implement About component and related CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Main from './components/pages/Main/Main'
 import MovieCatalog from './components/pages/MovieCatalog/MovieCatalog'
 import ShowsCatalog from './components/pages/ShowsCatalog/ShowsCatalog'
 import Navbar from './components/shared/Navbar/Navbar'
+import About from './components/pages/About/About'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/movies" element={<MovieCatalog />} />
         <Route path="/shows" element={<ShowsCatalog />} />
-        <Route path="/about" element={<MovieCatalog />} />
+        <Route path="/about" element={<About />} />
         <Route path="/details/:type/:id" element={<Details />} />
       </Routes>
     </Router>
---
+++ src/components/pages/About/About.tsx
@@ -0,0 +1,21 @@
+import styles from "./About.module.css";
+import CinemaItem from "../../shared/CinemaItem/CinemaItem";
+
+export default function About() {
+    return (
+        <main className={styles.mainContent}>
+            <h1 className={styles.title}>About MovieMe</h1>
+            <section className={styles.infoSection}>
+                <p>
+                    <b>MovieMe</b> is your go-to app for discovering popular movies and TV shows, watching trailers, and exploring detailed information about your favorite titles. Built with React, TypeScript, and Vite, MovieMe offers a modern, fast, and visually engaging experience. Enjoy features like search, ratings, and a beautiful catalog of cinema items!
+                </p>
+            </section>
+            <section className={styles.cinemaItemsSection}>
+                <h2 className={styles.cinemaItemsTitle}>Our Cinema Items</h2>
+                <div className={styles.cinemaItemsGrid}>
+                    TODO
+                </div>
+            </section>
+        </main>
+    );
+}
